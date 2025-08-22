1d9b5ddac041d190c5556d633ffbcd70c4dda24e
Jackson Luis <jacksonluis444@gmail.com>
2025-08-22 17:26:15 -0300

    Implementado CRUD para gerenciamento de usuários na API

--- src/main/java/dio/web/api/controller/WelcomeController.java
+++ src/main/java/dio/web/api/controller/WelcomeController.java
@@ -1,4 +1,12 @@
 package dio.web.api.controller;
 
-public class WellcomeController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome() {
+        return "Welcome to my Spring Boot Web API!";
+    }
 }
