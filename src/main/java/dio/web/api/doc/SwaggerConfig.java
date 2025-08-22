0e53f998439bb5635d1b32d22ee5abec9cb1b3eb
Jackson Luis <jacksonluis444@gmail.com>
2025-08-22 17:58:43 -0300

    Adicionado arquivo SwaggerConfig para configuração do Swagger na API

--- src/main/java/dio/web/api/doc/SwaggerConfig.java
+++ src/main/java/dio/web/api/doc/SwaggerConfig.java
@@ -1,4 +1,32 @@
 package dio.web.api.doc;
 
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.service.Contact;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+    private Contact contato(){
+        return new Contact(
+                "Seu nome",
+                "http://www.seusite.com.br",
+                "voce@seusite.com.br");
+    }
+    private ApiInfoBuilder informacoesApi(){
+
+        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+
+        apiInfoBuilder.title("Title - Rest API");
+        apiInfoBuilder.description("API exemplo de uso de Springboot REST API");
+        apiInfoBuilder.version("1.0");
+        apiInfoBuilder.termsOfServiceUrl("termo de uso: Open Source");
+        apiInfoBuilder.license("Licença - Sua Empresa ");
+        apiInfoBuilder.licenseUrl("http://www.seusite.com.br");
+        apiInfoBuilder.contact(this.contato());
+
+        return apiInfoBuilder;
+
+    }
 }
